d6ff4f75d22581aac6aea659957b0085308fdc67
Костя Шурыгин <kostiashuryhin@gmail.com>
2021-08-08 16:08:34 +0300

    Start Work

---
+++ HomeWork25/HomeWork25/DataModels/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  HomeWork25
+//
+//  Created by Константин on 8.08.21.
+//
+
+//import Foundation
+//import RealmSwift
+//
+//class Task: Object {
+//    @objc dynamic var name = ""
+//    @objc dynamic var note = ""
+//    @objc dynamic var data = Date()
+//    @objc dynamic var isComplete = false
+//}
---
+++ HomeWork25/HomeWork25/DataModels/TaskList.swift
@@ -0,0 +1,13 @@
+//
+//  TaskList.swift
+//  HomeWork25
+//
+//  Created by Константин on 8.08.21.
+//
+
+//import Foundation
+//import RealmSwift
+//
+//class TasksList: Object {
+//    <#code#>
+//}
